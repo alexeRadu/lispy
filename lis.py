81ecb48c5ac01ab1f2e1b050441f0018f9749e62
Radu Alexe <alexeRadu2007@gmail.com>
2017-05-11 23:03:46 +0300

    add test lisp code
    Signed-off-by: Radu Alexe <alexeRadu2007@gmail.com>

--- lis.py
+++ lis.py
@@ -1,3 +1,12 @@
 def tokenize(chars):
 	"Convert a string of characters into a list of tokens."
 	return chars.replace('(', ' (').replace(')', ' ) ').split()
+
+program = "\
+(begin\
+	(define r 10)\
+	(* pi (* r r)))"
+
+
+print program
+print tokenize(program)
