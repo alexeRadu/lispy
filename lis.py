4088839ef46af3622c787642ee84ca8564b6ac60
Radu Alexe <alexeRadu2007@gmail.com>
2017-05-12 17:07:30 +0300

    add procedures
    Signed-off-by: Radu Alexe <alexeRadu2007@gmail.com>

--- lis.py
+++ lis.py
@@ -3,9 +3,18 @@ import math
 import operator as op
 
 
+class Env(dict):
+	"An environment: a dict of {'var':val} pairs, with an outher Env."
+	def __init__(self, params = (), args = (), outer = None):
+		self.update(zip(params, args))
+		self.outer = outer
+
+	def find(self, var):
+		return self if (var in self) else self.outer.find(var)
+
 def standard_env():
 	"An environment with some Lisp standard procedures."
-	env = dict()
+	env = Env()
 	env.update(vars(math))
 	env.update({
 		'+': op.add,
@@ -57,12 +66,25 @@ def parse(program):
 	"Read a Lisp expression from a string."
 	return read_from_tokens(tokenize(program))
 
+class Procedure(object):
+	"A user-defined Lisp procedure."
+	def __init__(self, params, body, env):
+		self.params = params
+		self.body = body
+		self.env = env
+
+	def __call__(self, *args):
+		return eval(self.body, Env(self.params, args, self.env))
+
 def eval(x, env=global_env):
 	"Evaluate an expression in an environment."
 	if isinstance(x, str):		# variable reference
-		return env[x]
+		return env.find(x)[x]
 	elif not isinstance(x, list):	# constant literal
 		return x
+	elif x[0] == 'quote':		# quotation
+		(__, exp) = x
+		return exp
 	elif x[0] == 'if':		# conditional
 		(_, test, conseq, alt) = x
 		exp = (conseq if eval(test, env) else alt)
@@ -70,6 +92,12 @@ def eval(x, env=global_env):
 	elif x[0] == 'define':		# definition
 		(_, var, exp) = x
 		env[var] = eval(exp, env)
+	elif x[0] == 'set!':		# assignment
+		(_, var, exp) = x
+		env.find(var)[var] = eval(exp, env)
+	elif x[0] == 'lambda':		# procedure
+		(_, params, body) = x
+		return Procedure(params, body, env)
 	else:				# procedure call
 		proc = eval(x[0], env)
 		args = [eval(arg, env) for arg in x[1:]]
